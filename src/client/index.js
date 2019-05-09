11358bbfe325c4f20d0097bbaff3fa45d919100e
Connor Love <connor@dotconnor.com>
2019-05-09 04:36:25 -0400

    fix: react-dom render method

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./app"
 
-ReactDOM.mount(<App />, document.getElementById(`root`))
+ReactDOM.render(<App />, document.getElementById(`root`))
